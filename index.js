604724c2450b4a6d9973a19ffd4eb38f3f36a495
wulimax <1772003186@qq.com>
2019-01-26 21:02:27 +0800

    test

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 * @Author: 李政武
 * @Date:   2019-01-26 20:55:08
 * @Last Modified by:   李政武
-* @Last Modified time: 2019-01-26 20:56:09
+* @Last Modified time: 2019-01-26 21:02:00
 */
-console.log('测试')
+console.log('测试');
