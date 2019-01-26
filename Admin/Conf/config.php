2d2cf79a39bbecfcf5af576c1b403328646ea6af
wulimax <1772003186@qq.com>
2019-01-26 20:04:28 +0800

    -

--- Admin/Conf/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-return array(
-	//'配置项'=>'配置值'
-	//定义自定义特殊常量替换
-	'DEFAULT_MODULE'        =>  'Admin',  // 默认模块
-
-);
--- Home/Conf/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-return array(
-	//'配置项'=>'配置值'
-	'APP_GROUP_LIST' => 'Admin',
-	'DEFAULT_GROUP'  => 'Home',
-	'LAYOUT_ON'      =>  true,
-	
-);
